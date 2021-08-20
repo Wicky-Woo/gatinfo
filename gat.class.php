2a6aa27f724b8a185f505b36ef77a3c3bcd4d7ab
Wicky-Woo <88631005+Wicky-Woo@users.noreply.github.com>
2021-08-20 18:55:36 -0500

    remove tile cache, unused method

--- gat.class.php
+++ gat.class.php
@@ -14,7 +14,6 @@ class GAT
     protected $raw_data;
     protected $filesize;
     protected $tilecount;
-    protected $tiles = [];
     protected $walkable_tiles = 0;
     protected $wall_tiles = 0;
     protected $ledge_tiles = 0;
@@ -43,7 +42,6 @@ class GAT
         
         for($i = 0, $j = self::TILE_OFFSET; $i < $this->tilecount; $i++, $j += self::TILE_SIZE)
         {
-            $this->tiles[] = array_slice($this->raw_data, $j, self::TILE_SIZE);
             switch($this->raw_data[$j + self::TILE_TERRAIN_OFFSET])
             {
                 case self::TILE_WALKABLE:
@@ -86,29 +84,5 @@ class GAT
             "Wall" => $this->wall_tiles,
             "Ledge" => $this->ledge_tiles
         ];
-    }
-
-    //unneeded - this was my first pass through the GAT file format
-    function initialFileTest()
-    {
-        $filesize = filesize($this->path);
-        $fp = fopen($this->path, 'rb');
-        $binary = fread($fp, $filesize);
-        fclose($fp);
-
-        $unpacked = unpack(sprintf('C%d', $filesize), $binary);
-        $unpacked = array_values($unpacked); //reset array keys
-
-        $tilecount = ($filesize - self::TILE_OFFSET) / self::TILE_SIZE;
-
-        //should be dec equivalent to ascii "GRAT" (71826584)
-        //echo 'header: ' . $unpacked[0] . $unpacked[1] . $unpacked[2] . $unpacked[3];
-
-        $tiles = [];
-        
-        for($i = self::TILE_OFFSET; $i < $tilecount; $i += self::TILE_SIZE)
-        {
-            echo $unpacked[$i + 16] . $unpacked[$i + 17] . $unpacked[$i + 18] . $unpacked[$i + 19]. '<br />';
-        }
     }
 }
